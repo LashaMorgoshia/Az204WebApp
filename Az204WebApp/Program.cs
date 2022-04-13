ce1b4cd976475468db0f4d5c499c2e2899518708
LashaMorgoshia <lashamor@gmail.com>
2022-04-13 23:16:12 +0400

    Add image controller to upload files to AzStorage

--- Az204WebApp/Program.cs
+++ Az204WebApp/Program.cs
@@ -1,7 +1,10 @@
+using Az204WebApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.Configure<AzureStorageConfig>(builder.Configuration.GetSection("AzureStorageConfig"));
 
 var app = builder.Build();
 
